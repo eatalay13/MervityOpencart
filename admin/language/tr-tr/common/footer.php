e105673b501bc256c4f5f92cd1aba779932b9d21
Emrah Atalay <emrahatalay93@gmail.com>
2021-03-18 17:59:59 +0300

    first commit

---
+++ admin/language/tr-tr/common/footer.php
@@ -0,0 +1,6 @@
+<?php
+/* Turkceye Ceviren eka7a - http://e-piksel.com */
+
+// Text
+$_['text_footer']  = 'Mervity.com Copyright &copy; ' . date('Y') . ' Tüm Hakları Saklıdır.';
+$_['text_version'] = 'Sürüm %s';
